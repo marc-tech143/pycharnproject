6688be172e27098ec752f7578d64204e54a0782f
marc-tech <nnamaniifeanyi50@gmail.com>
2024-03-23 05:06:37 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2023 - int(birth_year)
+print(type(age))
+print(age)
+weight_lbs = input('weight(lbs): ')
+weight_kg = int(weight_lbs) * 0.45
+print(weight_kg)
+print(weight_lbs[0])
+course = 'python for Beginners'
+print(course[0])
